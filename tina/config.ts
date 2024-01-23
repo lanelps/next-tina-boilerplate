8b196eefb21931b2016054c1a5a90e5d46a85446
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-01-23 18:13:06 +1100

    Refactor TinaCMS config and schema

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "tinacms";
 
-import schema from "./src/schema";
-
 // Your hosting provider likely exposes this as an environment variable
 const branch =
   process.env.GITHUB_BRANCH ||
@@ -28,5 +26,32 @@ export default defineConfig({
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
-  schema,
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/edit/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
 });
--- tina/src/schema/index.js
+++
@@ -1,8 +0,0 @@
-import post from "./collections/post";
-
-const schema = {
-  collections: [post],
-  templates: [],
-};
-
-export default schema;
