464c678b3ea1f58a8c0678273bdb50a5788b8f66
Hamish McLean <hamishmclean@Hamishs-MBP.modem>
2024-01-30 15:28:09 +1100

    Review now available in Tina.

--- tina/src/schema/index.ts
+++ tina/src/schema/index.ts
@@ -1,5 +1,6 @@
 import { TinaCollection } from "tinacms";
 import { post } from "./collection/post";
+import { review } from "./collection/review"
 import { author } from "./collection/author";
 
-export const collections: TinaCollection[] = [post, author];
+export const collections: TinaCollection[] = [post, review, author];
