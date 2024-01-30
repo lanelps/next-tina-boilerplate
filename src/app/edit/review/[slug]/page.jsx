d2157102b21f11d21a3bdcfa8ac2c61ff69b2e07
Hamish McLean <hamishmclean@Hamishs-MBP.modem>
2024-01-30 15:20:05 +1100

    Add new files to create a review template.

---
+++ src/app/edit/review/[slug]/page.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTina } from "tinacms/dist/react";
+
+import ReviewTemplate from "../../../../templates/Review";
+import { getReviewPostData } from "../../../../utils/fetchData";
+
+const Review = ({ params }) => {
+  const [reviewData, setReviewData] = useState(null);
+
+  // Pass our data through the "useTina" hook to make it editable
+  const { data } = useTina({
+    query: reviewData?.query,
+    variables: reviewData?.variables,
+    data: reviewData?.data,
+  });
+
+  useEffect(() => {
+    const fetchReviewData = async () => {
+      const data = await getReviewPostData(params.slug);
+      setReviewData(data);
+    };
+
+    fetchReviewData();
+  }, [params.slug]);
+
+  return <ReviewTemplate data={data} />;
+};
+
+export default Review;
---
+++ src/templates/Review.jsx
@@ -0,0 +1,42 @@
+import { tinaField } from "tinacms/dist/react";
+
+import ContentSection from "../components/blog/ContentSection";
+import Blocks from "../components/blog/Blocks";
+
+const ReviewTemplate = ({ data }) => {
+  if (!data) return <></>;
+
+  const { title, body, sections } = data?.post;
+
+  return (
+    <div>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h1
+          className="m-8 text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl"
+          data-tina-field={tinaField(data, "title")}
+        >
+          {title}
+        </h1>
+        <ContentSection content={body} />
+        <Blocks blocks={sections} />
+      </div>
+      <div className="text-center bg-green-100">
+        Lost and looking for a place to start?
+        <a
+          href="https://tina.io/guides/tina-cloud/getting-started/overview/"
+          className="text-blue-500 underline"
+        >
+          {" "}
+          Check out this guide
+        </a>{" "}
+        to see how add TinaCMS to an existing Next.js site.
+      </div>
+    </div>
+  );
+};
+
+export default ReviewTemplate;
