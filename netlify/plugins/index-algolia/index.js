ebba6fdae702882fd26f8f5a7a47df1addbdbcee
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-01-31 17:37:09 +1100

    Refactor index-algolia plugin to use environment variables for Tina client ID

--- netlify/plugins/index-algolia/index.js
+++ netlify/plugins/index-algolia/index.js
@@ -7,8 +7,13 @@ dotenv.config({
   path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}.local`),
 });
 
-const MY_CLIENT_ID = process.env.NEXT_PUBLIC_TINA_CLIENT_ID;
 const MY_BRANCH = "main";
+
+if (process.env.NEXT_PUBLIC_TINA_CLIENT_ID || process.env.TINA_TOKEN) {
+  throw new Error(
+    "The NEXT_PUBLIC_TINA_CLIENT_ID environment variable is not set"
+  );
+}
 
 const getTinaGraphQLVersion = async () => {
   const pkgPath = path.join(
@@ -21,10 +26,33 @@ const getTinaGraphQLVersion = async () => {
   return pkg.version.split(".").slice(0, 2).join(".");
 };
 
+const fetchBlogPosts = async (url, headers, query) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      query,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const { data } = await response.json();
+
+  return (
+    data?.postConnection?.edges?.map(({ node }) => ({
+      objectID: node.id,
+      title: node.title,
+    })) ?? []
+  );
+};
+
 const getAllBlogPosts = async () => {
   try {
     const tinaGraphQLVersion = await getTinaGraphQLVersion();
-    const url = `https://content.tinajs.io/${tinaGraphQLVersion}/content/${MY_CLIENT_ID}/github/${MY_BRANCH}`;
+    const url = `https://content.tinajs.io/${tinaGraphQLVersion}/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${MY_BRANCH}`;
 
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
@@ -43,26 +71,7 @@ const getAllBlogPosts = async () => {
       }
     `;
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers,
-      body: JSON.stringify({
-        query,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const { data } = await response.json();
-
-    return (
-      data?.postConnection?.edges?.map(({ node }) => ({
-        objectID: node.id,
-        title: node.title,
-      })) ?? []
-    );
+    return await fetchBlogPosts(url, headers, query);
   } catch (error) {
     console.error("An error occurred while fetching blog posts:", error);
     return [];
