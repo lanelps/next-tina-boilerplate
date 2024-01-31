095bac1fbdd7c00fb7ce4eaebd159583c9312204
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-01-31 20:47:33 +1100

    Add remotePatterns for Cloudinary images and use Next.js Image component in Hero section

--- src/components/blocks/Hero.jsx
+++ src/components/blocks/Hero.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = ({ data }) => {
   return (
-    <section className="p-4 bg-blue-500 text-white">
+    <section className="p-4 text-white bg-blue-500">
       <h3 className="text-xl font-bold">Hero section</h3>
-      <h2 className="text-2xl font-bold mb-2">{data.headline}</h2>
+      <Image
+        className="object-cover w-full aspect-video"
+        width={1024}
+        height={576}
+        src={data.heroImage}
+        alt="Hero image"
+      />
+      <h2 className="mb-2 text-2xl font-bold">{data.headline}</h2>
       <span className="font-semibold">{data.tagline}</span>
       <p className="mb-4">{data.text}</p>
     </section>
