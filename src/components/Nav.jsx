9cb13a8e447586a76dd2d57237f0e3b391265393
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-02-06 11:37:36 +1100

    Refactor layout and add navigation component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <header className="fixed top-0 w-full h-[5.5rem]">
+      <nav aria-label="Main Navigation">
+        <ul className="flex items-center gap-4 p-8">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
