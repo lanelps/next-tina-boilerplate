513997adcaeb7e3df919fa8edb06a7c5b7b83bf9
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-02-06 15:50:13 +1100

    Refactor Blocks component to use optional chaining

--- src/components/blog/Blocks.jsx
+++ src/components/blog/Blocks.jsx
@@ -5,35 +5,34 @@ import Features from "../blocks/Features";
 import Hero from "../blocks/Hero";
 // import { Testimonial } from "./blocks/testimonial";
 
-const Blocks = ({ blocks }) => (
-  <section className="w-full my-4 py-4 border-black border-t border-b flex flex-col gap-8">
-    {blocks
-      ? blocks.map((block, i) => {
-          switch (block.__typename) {
-            case "PostSectionsContent":
-              return (
-                <React.Fragment key={i + block.__typename}>
-                  <Content data={block} />
-                </React.Fragment>
-              );
-            case "PostSectionsHero":
-              return (
-                <React.Fragment key={i + block.__typename}>
-                  <Hero data={block} />
-                </React.Fragment>
-              );
-            case "PostSectionsFeatures":
-              return (
-                <React.Fragment key={i + block.__typename}>
-                  <Features data={block} />
-                </React.Fragment>
-              );
-            default:
-              return null;
-          }
-        })
-      : null}
-  </section>
-);
+const Blocks = ({ blocks }) =>
+  blocks?.length > 0 ? (
+    <section className="flex flex-col w-full gap-8 py-4 my-4 border-t border-b border-black">
+      {blocks.map((block, i) => {
+        switch (block.__typename) {
+          case "PostSectionsContent":
+            return (
+              <React.Fragment key={i + block.__typename}>
+                <Content data={block} />
+              </React.Fragment>
+            );
+          case "PostSectionsHero":
+            return (
+              <React.Fragment key={i + block.__typename}>
+                <Hero data={block} />
+              </React.Fragment>
+            );
+          case "PostSectionsFeatures":
+            return (
+              <React.Fragment key={i + block.__typename}>
+                <Features data={block} />
+              </React.Fragment>
+            );
+          default:
+            return null;
+        }
+      })}
+    </section>
+  ) : null;
 
 export default Blocks;
