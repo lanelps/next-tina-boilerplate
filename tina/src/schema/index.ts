7872cfd3b2d2f574e8440dae39f15faa3bb39e7a
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-02-06 15:52:50 +1100

    Delete unused review-related files and update schema

--- tina/src/schema/index.ts
+++ tina/src/schema/index.ts
@@ -1,6 +1,5 @@
 import { TinaCollection } from "tinacms";
 import { post } from "./collection/post";
-import { review } from "./collection/review"
 import { author } from "./collection/author";
 
-export const collections: TinaCollection[] = [post, review, author];
+export const collections: TinaCollection[] = [post, author];
